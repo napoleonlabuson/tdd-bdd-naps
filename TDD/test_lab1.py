3efd06d8d87390d347717beca8b5d29184643701
Napoleon Labuson <napoleon.labuson@globe.com.ph>
2022-07-21 12:25:06 +0000

    Test Lab 1 and 2

---
+++ TDD/test_lab1.py
@@ -0,0 +1,11 @@
+def increment_by_one(i):
+    return i + 1
+    
+def decrement_by_one(i):
+    return i - 1
+    
+def test_answer1():
+    assert increment_by_one(1) == 2
+
+def test_answer2():
+    assert decrement_by_one(1) == 0
